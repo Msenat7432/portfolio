bf194ecafe0fd709bb5c9bf76f0f3b78f77d5378
msenat7432 <maxsenat7432@gmail.com>
2021-05-06 16:48:55 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/nav'
+
+
 function App() {
   return (
     <div className="App">
-    
+      <Navbar/>
+     
+
+
+
+
+
     </div>
   );
 }
